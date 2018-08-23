341a39a709fcad02f7ed8ddfa90c50cfab6ec992
dabush <d.marle.bush@gmail.com>
2018-08-23 15:40:53 -0700

    added slick and new fields to books

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, Theme, Genre, Form, Period
 
 admin.site.register(Book)
+admin.site.register(Theme)
+admin.site.register(Genre)
+admin.site.register(Form)
+admin.site.register(Period)
--- books/models.py
+++ books/models.py
@@ -1,6 +1,42 @@
 from django.db import models
 from django_countries.fields import CountryField
 
+class Theme(models.Model):
+	theme_name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.theme_name
+
+	class Meta:
+		ordering = ('theme_name',)
+
+class Genre(models.Model):
+	genre_name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.genre_name
+
+	class Meta:
+		ordering = ('genre_name',)
+
+class Form(models.Model):
+	form_name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.form_name
+
+	class Meta:
+		ordering = ('form_name',)
+
+class Period(models.Model):
+	period_name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.period_name
+
+	class Meta:
+		ordering = ('period_name',)
+
 class Book(models.Model):
 	book_title = models.CharField(max_length=200)
 	book_subtitle = models.CharField(max_length=200, blank=True)
@@ -10,8 +46,16 @@ class Book(models.Model):
 	author = models.ForeignKey('authors.Author', on_delete=models.CASCADE,)
 	cover_image = models.ImageField(null=True, blank=True, upload_to='book_images')
 	slug = models.SlugField(max_length=30)
+	themes = models.ManyToManyField(Theme, blank=True)
+	genres = models.ManyToManyField(Genre, blank=True)
+	forms = models.ManyToManyField(Form, blank=True)
+	periods = models.ManyToManyField(Period, blank=True)
 	book_desc = models.TextField()
 	book_country = CountryField()
 	book_featured = models.BooleanField()
+
 	def __str__(self):
 		return self.book_title
+
+	class Meta:
+		ordering = ('book_title',)
