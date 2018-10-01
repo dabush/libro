49742d0dc547cedfc8a0767ba11c13ec4f04d56a
dabush <d.marle.bush@gmail.com>
2018-10-01 12:34:24 -0700

    rich text on detail pages

--- mysite/localsettings.py
+++ mysite/localsettings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'social_django',
     'rest_framework',
     'django_filters',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
@@ -138,6 +139,34 @@ AUTHENTICATION_BACKENDS = [
     'social_core.backends.google.GoogleOAuth2',
 ]
 
+TINYMCE_DEFAULT_CONFIG = {
+    'height': 360,
+    'width': 1120,
+    'cleanup_on_startup': True,
+    'custom_undo_redo_levels': 20,
+    'selector': 'textarea',
+    'theme': 'modern',
+    'plugins': '''
+            textcolor save link image media preview codesample contextmenu
+            table code lists fullscreen  insertdatetime  nonbreaking
+            contextmenu directionality searchreplace wordcount visualblocks
+            visualchars code fullscreen autolink lists  charmap print  hr
+            anchor pagebreak
+            ''',
+    'toolbar1': '''
+            fullscreen preview bold italic underline | fontselect,
+            fontsizeselect  | forecolor backcolor | alignleft alignright |
+            aligncenter alignjustify | indent outdent | bullist numlist table |
+            | link image media | codesample |
+            ''',
+    'toolbar2': '''
+            visualblocks visualchars |
+            charmap hr pagebreak nonbreaking anchor |  code |
+            ''',
+    'contextmenu': 'formats | link image',
+    'menubar': True,
+    'statusbar': True,
+    }
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
